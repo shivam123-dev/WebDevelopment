d5e1c4e259d2b066f90cae38f32f061c3bf56394
shivam123-dev <ss2249@srmist.edu.in>
2021-05-25 23:32:52 +0530

    Creating first express app

--- express(tut70-71)/app.js
+++ express(tut70-71)/app.js
@@ -3,10 +3,25 @@
 const express = require('express');
 const app = express();
 const port = 80;
+// app.get('/', (req, res) => {
+//     res.send('This is home page of my first express app');
+// });
+
+// for sending the status code
 app.get('/', (req, res) => {
-    res.send('This is my first express app');
+    res.status(200).send('This is home page of my first express app');
 });
 
+app.get('/about', (req, res) => {
+    res.send('This is about page of my first express app');
+});
+app.post('/about', (req, res) => {
+    res.send('This is post');
+});
+app.get('/this', (req, res) => {
+    res.status(404).send('Cannot get this');
+});
+// Most important
 app.listen(port, () => {
     console.log(`${port}`);
 })
