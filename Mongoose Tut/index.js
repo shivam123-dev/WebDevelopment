4de2b18bb775477cf9dee320e5ae4e8bf449c2ac
shivam123-dev <ss2249@srmist.edu.in>
2021-06-03 14:30:53 +0530

    Getting started with mongoose

--- Mongoose Tut/index.js
+++ Mongoose Tut/index.js
@@ -4,7 +4,43 @@ mongoose.connect('mongodb://localhost/shivamKart', { useNewUrlParser: true, useU
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    // we're connected!
-    console.log("We are connected");
+// db.once('open', function () {
+//     // we're connected!
+//     console.log("Connected");
+// });
+
+// Creating a new schema
+var kittySchema = new mongoose.Schema({
+    name: String
 });
+
+kittySchema.methods.speak = function () {
+    var greeting = "My name is " + this.name;
+    console.log(greeting);
+}
+
+
+// locking a schema means converting schema into a model
+var Kitten = mongoose.model('shivamKitty', kittySchema);
+// creating object for schema
+var shivamKitty = new Kitten({name: "shivamKitty"});
+var shivamKitty2 = new Kitten({name: "shivamKitty2"});
+// console.log(shivamKitty);
+// console.log(shivamKitty.name);
+// shivamKitty.speak();
+
+// for saving in mongo db
+shivamKitty.save(function (err, shivamKitty) {
+    if (err) return console.error(err);
+        // shivamKitty.speak();
+});
+shivamKitty2.save(function (err, k) {
+    if (err) return console.error(err);
+        // k.speak();
+});
+
+// for find operation
+Kitten.find({name: "shivamKitty2"},function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+})
